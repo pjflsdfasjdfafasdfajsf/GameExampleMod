d64dd96a258ae5ffef19e1498424bcee65ff3fea
pjflsdfasjdfafasdfajsf <peppohappy8@gmail.com>
2026-06-25 19:55:31 +0300

    Update Build.sh

--- Build.sh
+++ Build.sh
@@ -1,37 +1,43 @@
 #!/bin/sh
 #
 # NOTE: Linux compilation script for ExampleMod
-# TODO: Make it generic for any mods and then just execute it from the game?
 # 
+set -e
 
-if [ $# -lt 1 ]; then
-    printf "You must provide the path to SDK headers: ./Build.sh Path/To/Headers\n"
+# TODO: Distribute libSDK.so instead and link to it dynamically.
+if [ $# -lt 2 ]; then
+    printf "You must provide the path to SDK headers and libSDK_wasm.a:\n"
+    printf "Usage: ./Build.sh Path/To/Headers Path/To/libSDK_wasm.a\n"
     exit 1
 fi
 
-if [ ! -d "$1" ]; then
-    printf "Error: '$1' is not a valid directory.\n"
+Headers="$1"
+Lib="$2"
+
+if [ ! -d "${Headers}" ]; then
+    printf "Error: '${Headers}' is not a valid directory.\n"
     exit 1
 fi
 
-SDK="$1"
+if [ ! -f "${Lib}" ]; then
+    printf "Error: '${Lib}' is not a valid file.\n"
+    exit 1
+fi
+
 Compiler="clang"
 
 # NOTE: Dirs
-
 BuildDir="Build"
-
 mkdir -p ${BuildDir}
 
 # NOTE: This is for printing.
 Align="%-15s"
 
 printf ${Align} "ExampleMod"
-ExampleModSrc="Code/ExampleMod.c        \
-               ../../Code/Public/Init.c
-              "
+ExampleModSrc="Code/ExampleMod.c"
 ExampleModTarget="${BuildDir}/ExampleMod.wasm"
-ExampleModFlags="-I${SDK} --target=wasm32 -nostdlib -Wl,--no-entry -Wl,--export-all -Wl,--allow-undefined"
+ExampleModFlags="-I${Headers} --target=wasm32 -nostdlib -Wl,--no-entry -Wl,--export-all -Wl,--allow-undefined"
 
-${Compiler} ${ExampleModFlags} ${ExampleModSrc} -o ${ExampleModTarget}
+${Compiler} ${ExampleModFlags} ${ExampleModSrc} -Wl,--whole-archive "${Lib}" -Wl,--no-whole-archive -o ${ExampleModTarget}
+
 printf "Done\n"
